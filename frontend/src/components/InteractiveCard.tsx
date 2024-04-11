38ab95ff39c612745559c376fefb05e69523f8b8
phuminun <6633192021@student.chula.ac.th>
2024-04-11 22:06:35 +0700

    interactive coupon

--- frontend/src/components/InteractiveCard.tsx
+++ frontend/src/components/InteractiveCard.tsx
@@ -5,17 +5,17 @@ export default function InteractiveCard( { children, contentName } : { children:
 
     function onCardMouseAction(event:React.SyntheticEvent) {
         if( event.type == 'mouseover') {
-            event.currentTarget.classList.remove( 'shadow-xl')
-            event.currentTarget.classList.add( 'shadow-3xl')
+            event.currentTarget.classList.remove( 'shadow-xl', 'scale-100')
+            event.currentTarget.classList.add( 'shadow-3xl', 'scale-110')
         }
         else {
-            event.currentTarget.classList.remove( 'shadow-3xl')
-            event.currentTarget.classList.add( 'shadow-xl')
+            event.currentTarget.classList.remove( 'shadow-3xl', 'scale-110')
+            event.currentTarget.classList.add( 'shadow-xl', 'scale-100')
         }
     }
 
     return (
-        <div className='w-1/5 h-[300px] rounded-lg shadow-xl bg-white' 
+        <div className='w-1/5 h-[300px] rounded-lg shadow-xl bg-white transform transition-transform duration-500 ease-in-out scale-100' 
         onMouseOver={ (e) => onCardMouseAction(e)}
         onMouseOut={(e)=> onCardMouseAction(e)} >
             { children }

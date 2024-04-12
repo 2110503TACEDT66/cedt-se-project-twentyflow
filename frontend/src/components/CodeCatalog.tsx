f6c4460ed161c0eec2dbc28723c6e75bf4d31100
phuminun <6633192021@student.chula.ac.th>
2024-04-12 16:06:17 +0700

    mycoupon fetch real data

--- frontend/src/components/CodeCatalog.tsx
+++ frontend/src/components/CodeCatalog.tsx
@@ -1,12 +1,41 @@
+"use client"
+
+import { useState, useEffect } from 'react';
 import Code from "./Code"
+import getUserProfile from '@/libs/getUserProfile';
+import { useSession } from 'next-auth/react';
+
+
+interface Coupon {
+    couponName: string;
+    couponCode: string;
+    // add other properties as needed
+}
+interface UserData {
+    coupons: Coupon[];
+    // add other properties as needed
+}
+
+interface User {
+    data: UserData;
+    // add other properties as needed
+}
 
 export default function CodeCatalog(){
-    const userCoupons = ["asdfjkl;lkjfdsa", "qwertyuiop", "zxcvbnm"]; // replace this with actual user coupons
+    const [user, setUser] = useState<User|null>(null);
+    const {data: session} = useSession();
+    const token = session?.user?.token as string;
+
+    useEffect(() => {
+        getUserProfile(token).then(userData => {
+            setUser(userData);
+        });
+    }, []);
 
     return(
         <div className=" flex flex-col space-y-5 p-[50px]">
-            {userCoupons.map((couponCode, index) => (
-                <Code key={index} couponName={`Coupon Discount 50 THB ${index + 1}`} couponCode={couponCode}/>       
+            {user?.data.coupons.map((coupon) => (
+                <Code couponName={coupon.couponName} couponCode={coupon.couponCode}/>       
             ))}
         </div>
     )

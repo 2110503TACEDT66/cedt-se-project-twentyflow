c277f99b835c1c1193f369bdb32f5397a4296d11
Schwynn Sant <sschwynn@gmail.com>
2024-04-14 15:18:55 +0700

    Delete appointment

---
+++ frontend/src/components/CustomerDashboard.tsx
@@ -0,0 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
+export default function CustomerDashboard() {
+
+    return(
+        <div className="flex flex-row rounded-lg">
+            <div className="flex flex-col">
+                
+            </div>
+        </div>
+    )
+}

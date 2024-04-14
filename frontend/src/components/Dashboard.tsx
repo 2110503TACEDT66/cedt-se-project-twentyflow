2a4067647b64a5169945a580b8658f3c8fee1d87
Toptennn <topten.watthana@gmail.com>
2024-04-15 03:17:20 +0700

    fix dashboard

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,10 +1,12 @@
 import StatCard from "./StatCard";
 import ChartBar from "./ChartBar";
+import ChartBarCalendar from "./ChartBarCalendar";
 
 export default function Dashboard() {
     return (
         <div>
             <StatCard/>
+            <ChartBarCalendar/>
             <ChartBar/>
         </div>
     )

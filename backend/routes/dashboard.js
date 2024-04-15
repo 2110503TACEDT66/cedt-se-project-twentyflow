bdb275bbfea987d45c52c7331eb21de11639e3c4
Schwynn Sant <sschwynn@gmail.com>
2024-04-15 12:35:20 +0700

    add trend

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,10 +1,13 @@
 const express = require('express');
-const {getDashboard} = require('../controllers/dashboard');
+const {getDashboard, getCustomerMonthTrend, getCustomerDailyTrend, getCustomerThisMonth} = require('../controllers/dashboard');
 
 const {protect, authorize} = require('../middleware/auth');
 
 const router = express.Router();
 
 router.route('/').get(protect,authorize('admin'),getDashboard)
+router.route('/customer').get(protect, authorize('admin'), getCustomerThisMonth)
+router.route('/customer/monthly').get(protect,authorize('admin'), getCustomerMonthTrend)
+router.route('/customer/daily').get(protect,authorize('admin'), getCustomerDailyTrend)
 
 module.exports = router

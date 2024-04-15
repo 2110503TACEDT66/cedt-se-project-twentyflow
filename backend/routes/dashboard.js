ac0f5dd4f8da94de10f5a10cd9d97dc35e788e5b
Schwynn Sant <sschwynn@gmail.com>
2024-04-15 21:14:54 +0700

    Get Revenue

--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getDashboard, getCustomerMonthTrend, getCustomerDailyTrend, getCustomerThisMonth, getYearlyRevenue} = require('../controllers/dashboard');
+const {getDashboard, getCustomerMonthTrend, getCustomerDailyTrend, getCustomerThisMonth, getYearlyRevenue, getActiveUser, getNewReturnCustomer, getWeeklyRevenue} = require('../controllers/dashboard');
 
 const {protect, authorize} = require('../middleware/auth');
 
@@ -7,8 +7,11 @@ const router = express.Router();
 
 router.route('/').get(protect,authorize('admin'),getDashboard)
 router.route('/customer').get(protect, authorize('admin'), getCustomerThisMonth)
-                        .get('/monthly',protect,authorize('admin'), getCustomerMonthTrend)
-                        .get('/daily',protect,authorize('admin'), getCustomerDailyTrend)
-router.route('/revenue').get('/yearly',protect, authorize('admin'), getYearlyRevenue)
+router.route('/customer/monthly').get(protect,authorize('admin'), getCustomerMonthTrend)
+router.route('/customer/daily').get(protect,authorize('admin'), getCustomerDailyTrend)
+router.route('/customer/active').get(protect,authorize('admin'), getActiveUser)
+//router.route('/customer/newReturn').get(protect,authorize('admin'), getNewReturnCustomer)
+router.route('/revenue/yearly').get(protect, authorize('admin'), getYearlyRevenue)
+router.route('/revenue/weekly').get(protect, authorize('admin'), getWeeklyRevenue)
 
 module.exports = router

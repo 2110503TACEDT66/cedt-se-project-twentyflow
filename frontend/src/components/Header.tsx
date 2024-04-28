7e35b4943c32c03fcf36c88d4be91113306a567b
Pongsapat Methasate <pongsapatmethasate@gmail.com>
2024-04-28 16:34:52 +0700

    responsive kiang

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import Link from "next/link";
@@ -5,23 +6,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faScrewdriverWrench } from "@fortawesome/free-solid-svg-icons";
 import getUserProfile from "@/libs/getUserProfile";
 import LogOutButton from "./LogOutButton";
-import { useRouter } from "next/navigation";
-import Swal from "sweetalert2";
-import LogInButton from "./LogInButton";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from 'react';
+import { useSession } from "next-auth/react";
 
-export default async function Header() {
-  const session = await getServerSession(authOptions);
-  // var profile = null
-  // if (session) {
-  //     var profile = await getUserProfile(session.user.token)
-  // }
+
+export default function Header() {
+  const { data: session } = useSession();    
   
+  // State to manage the visibility of the menu
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  // Function to toggle the menu
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+  
+
   return (
     <header className="flex items-center justify-between py-7 px-10 h-[10vh]">
       <Link href="/" className="text-5xl font-black text-main-100">
       CEDT
       </Link>
-      <nav>
+      <div className="md:hidden block">
+        <FontAwesomeIcon icon={menuOpen ? faTimes : faBars} className="w-6 text-main-100 cursor-pointer" onClick={toggleMenu} />
+      </div>
+      <nav className={`flex ${menuOpen ? 'flex-col' : 'hidden'} md:flex`}>
         <div className="flex space-x-7 justify-center items-center">
           <Link href="/" className=" text-xl font-bold hover:text-main-100">
             HOME
@@ -61,6 +71,7 @@ export default async function Header() {
               <Link href="/booking" className=" text-xl font-bold hover:text-main-100">
                 BOOKING
               </Link>
+              
               <Link
                 className="text-xl flex flex-row font-bold space-x-2 text-white p-2 bg-main-100 rounded-md"
                 href="/account"
@@ -69,6 +80,7 @@ export default async function Header() {
                 <h1>{session.user.name}</h1>
               </Link>
               <LogOutButton />
+              
             </div>
           )}
         </div>
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -1,35 +1,43 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Main() {
-    return(
-        <div className=" w-full flex flex-row h-[90vh]">
-            <div className=" w-2/6 p-10 h-full flex flex-col space-y-5 justify-center">
+    return (
+        <div className="w-full flex flex-col md:flex-row h-[90vh]">
+            <div className="w-full p-10 h-full flex flex-col space-y-6 md:h-full md:flex-col md:space-y-6 md:justify-center md:w-1/2">
                 <div>
-                    <h1 className=" text-5xl text-main-100 font-extrabold">
+                    <h1 className="text-5xl text-main-100 font-extrabold text-center">
                         COWORKING SPACE
                     </h1>
                 </div>
+                <div className="md:h-full md:hidden block h-3/4">
+                <Image
+                    src="/img/bg1.svg"
+                    alt="bg"
+                    width={0}
+                    height={0}
+                    className="object-cover w-full h-full"
+                />
+                </div>
                 <div>
-                    <h1 className=" font-semibold text-zinc-600 font-kanit">
+                    <h1 className="font-semibold text-zinc-600 font-kanit text-center md:text-left">
                         ปลดล็อกศักยภาพของคุณ Coworking Space ที่ออกแบบมาเพื่อการทำงานอย่างมีประสิทธิภาพ
                     </h1>
                 </div>
-                <Link href={"/coworkings"} className=" bg-main-100 text-white  text-center py-3 rounded-md font-semibold">
+                <Link href="/coworkings" className="bg-main-100 text-white text-center py-3 rounded-md font-semibold">
                     RESERVE
                 </Link>
-
             </div>
-            <div className=" w-4/6 h-full">
-                <Image 
-                src={"/img/bg1.svg"}
-                alt="bg"
-                width={0}
-                height={0}
-                unoptimized
-                className=" object-fill w-full h-full"
+            <div className="w-full md:w-4/6 md:h-full md:block hidden">
+                <Image
+                    src="/img/bg1.svg"
+                    alt="bg"
+                    width={0}
+                    height={0}
+                    unoptimized
+                    className="object-fill w-full h-full"
                 />
             </div>
         </div>
-    )
+    );
 }
